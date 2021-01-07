7cf0cde321614c20196d9134ad9001dc6a80da56
wtarmstrong <72756413+wtarmstrong@users.noreply.github.com>
2021-01-07 14:15:20 -0600

    Adding Quiz 17

---
+++ Quiz_17/quiz_17.js
@@ -0,0 +1,69 @@
+/* Change these items for each quiz
+ * Other than the items in this file, the only things that will need to be changed for each quiz are the images in the Images folder and the reference to this document in the HTML file.
+ * The part to change in the HTML file is near the bottom. 
+ * It looks like this: <script src="quiz_#.js"></script> 
+ * Just change the # to the number of the quiz. */
+
+// This text is what is displayed on the tab of the web page.
+var tabTitle = "Map 17: The Enlightenment and the American Revolution";
+
+// This text is what is displayed at the top of the web page
+var quizHeader = "Geography Quiz 17: Important Locations in the Colonies";
+
+// This is used to change the size of the div that has the map images. The first value is the map's width; the second is its height.
+var imgDimension = [1251, 846];
+
+// This is used to change the areas used in the image map "imgClick"
+// For each item, "x1, y1, x2, y2" is used to create the rect areas of the image map.
+var mapDimension = [
+  /* A */ " 939,  513,  975,  552",
+  /* B */ " 554,  474,  659,  545",
+  /* C */ " 947,  414, 1004,  453",
+  /* D */ " 864,  704,  918,  752",
+  /* E */ " 821,  752,  872,  797",
+  /* F */ "1025,  329, 1076,  377",
+  /* G */ " 986,  366, 1034,  410",
+  /* H */ " 903,  473,  944,  512",
+  /* I */ "1106,  246, 1158,  294",
+  /* J */ "1007,  492, 1083,  551"];
+
+// This text is displayed next to "Selected Question: " when the associated question is selected. The text comes from the original quiz.
+var qTextArray = [
+  /* 1. */  "Baltimore",
+  /* 2. */  "Richmond",
+  /* 3. */  "Boston",
+  /* 4. */  "Charles Town",
+  /* 5. */  "Savannah",
+  /* 6. */  "Philadelphia, location of the Continental Congress",
+  /* 7. */  "Mississippi River, the new nation's western border, as set by the Treaty of Paris",
+  /* 8. */  "New York, a major colonial city and commercial center",
+  /* 9. */  "Chesapeake Bay, blockaded by a French fleet at the end of the American Revolution",
+  /* 10. */ "Virginia, home colony of Declaration of Independence author Thomas Jefferson"];
+
+// At this time, this array is not used for anything other than supplying the value for qArray[i].answer. No function currently exists to grade the quiz as to avoid giving the answers to the student.
+// Enter the letter representing the correct location for each question.
+var qAnsArray = [
+  /*  1. */  "C",
+  /*  2. */  "A",
+  /*  3. */  "I",
+  /*  4. */  "D",
+  /*  5. */  "E",
+  /*  6. */  "G",
+  /*  7. */  "B",
+  /*  8. */  "F",
+  /*  9. */  "J",
+  /*  10. */ "H"
+]
+
+// This text is displayed next to "Selected Location: " when the associated location is selected. The intention is to assist the student in finding the location on the map.
+var lTextArray = [
+  /* A. */ "A is a city near the middle of the 13 colonies. It is in the region H and near the body of water J.",
+  /* B. */ "B is a river in the western portion of the map.",
+  /* C. */ "C is a city near the middle of the 13 colonies. It is northeast of A and H, southwest of G, and veryc close to the body of water J.",
+  /* D. */ "D is a city in the southern portion of the 13 colonies. It is northeast of city E.",
+  /* E. */ "E is a city in the southern portion of the 13 colonies. It is southwest of city D.",
+  /* F. */ "F is a city in the northeast portion of the 13 colonies. It is between cities G and I.",
+  /* G. */ "G is a city in the northeast portion of the 13 colonies. It is between cities C and F.",
+  /* H. */ "H is a region in the middle of the 13 colonies.",
+  /* I. */ "I is a city in the northeast portion of the 13 colonies. It is northeast of city F.",
+  /* J. */ "J is a small body of water near the middle of the 13 colonies."];
